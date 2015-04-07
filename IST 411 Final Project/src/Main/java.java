be5b743bf9c602e821b61f1b315fce9c1596a371
camruny <camruny@me.com>
2015-04-07 19:05:16 -0400

    Test

--- IST 411 Final Project/src/Main/java.java
+++ IST 411 Final Project/src/Main/java.java
@@ -15,7 +15,7 @@ public class java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // This is the beginning of our app
     }
     
 }
